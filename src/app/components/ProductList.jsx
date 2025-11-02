b13ff65be6888ca0c6b7adf25a9bb0b1691ec9a8
Laura <lauracastanovilla@usf.edu>
2025-11-02 17:08:03 -0500

    changing const for makeup and changing id's

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -1,9 +1,9 @@
 'use client';
 
-const products = [
-    {id: 1, name: 'Lipstick', price: 27.00, category: 'Makeup', stock: 24},
-    {id: 2, name: 'Mascara', price: 29.00, category: 'Makeup', stock: 30},
-    {id: 3, name: 'Eyeshadow Palette', price: 54.00, category: 'Makeup', stock: 19},
-    {id: 4, name: 'Concealer', price: 20.00, category: 'Makeup', stock: 12},
-    {id: 5, name: 'Blush', price: 26.00, category: 'Makeup', stock: 30},
+const makeupProducts = [
+    {id: 'm-1', name: 'Lipstick', price: 27.00, category: 'Makeup', stock: 24},
+    {id: 'm-2', name: 'Mascara', price: 29.00, category: 'Makeup', stock: 30},
+    {id: 'm-3', name: 'Eyeshadow Palette', price: 54.00, category: 'Makeup', stock: 19},
+    {id: 'm-4', name: 'Concealer', price: 20.00, category: 'Makeup', stock: 12},
+    {id: 'm-5', name: 'Blush', price: 26.00, category: 'Makeup', stock: 30},
 ]
