6f81ed95cd71e80332e89ed756f02f06cfd8a7f1
Laura <lauracastanovilla@usf.edu>
2025-11-02 17:05:49 -0500

    creating components and starting product arrays

---
+++ src/app/components/ProductList.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+const products = [
+    {id: 1, name: 'Lipstick', price: 27.00, category: 'Makeup', stock: 24},
+    {id: 2, name: 'Mascara', price: 29.00, category: 'Makeup', stock: 30},
+    {id: 3, name: 'Eyeshadow Palette', price: 54.00, category: 'Makeup', stock: 19},
+    {id: 4, name: 'Concealer', price: 20.00, category: 'Makeup', stock: 12},
+    {id: 5, name: 'Blush', price: 26.00, category: 'Makeup', stock: 30},
+]
