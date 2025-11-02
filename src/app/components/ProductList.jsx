fb6afd7a45ee28a6142adad529823084cc777350
Laura <lauracastanovilla@usf.edu>
2025-11-02 17:31:04 -0500

    putting all products under one const, created ProductList function

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -1,6 +1,7 @@
 'use client';
 
-const makeupProducts = [
+const products = [
+    // Makeup Products
     {id: 'm-1', name: 'Lipstick', price: 27.00, category: 'Makeup', stock: 24},
     {id: 'm-2', name: 'Mascara', price: 29.00, category: 'Makeup', stock: 30},
     {id: 'm-3', name: 'Eyeshadow Palette', price: 54.00, category: 'Makeup', stock: 19},
@@ -9,9 +10,7 @@ const makeupProducts = [
     {id: 'm-6', name: 'Skin Tint', price: 32.00, category: 'Makeup', stock: 42},
     {id: 'm-7', name: 'Highligher', price: 17.00, category: 'Makeup', stock: 28},
     {id: 'm-8', name: 'Bronzer', price: 21.00, category: 'Makeup', stock: 15},
-];
-
-const skincareProducts = [
+    // Skincare Products
     {id: 's-1', name: 'Cleansing Balm', price: 54.00, category: 'Skincare', stock: 20},
     {id: 's-2', name: 'Moisturizer', price: 26.99, category: 'Skincare', stock: 28},
     {id: 's-3', name: 'Eye Cream', price: 54.00, category: 'Skincare', stock: 10},
@@ -20,9 +19,7 @@ const skincareProducts = [
     {id: 's-6', name: 'Face Wash', price: 21.99, category: 'Skincare', stock: 40},
     {id: 's-7', name: 'Sunscreen', price: 38.00, category: 'Skincare', stock: 35},
     {id: 's-8', name: 'Chemical Exfoliator', price: 29.99, category: 'Skincare', stock: 11},
-];
-
-const hairProducts = [
+    // Hair Products
     {id: 'h-1', name: 'Straightener', price: 239.99, category: 'Hair', stock: 32},
     {id: 'h-2', name: 'Shampoo', price: 34.00, category: 'Hair', stock: 17},
     {id: 'h-3', name: 'Conditioner', price: 17.99, category: 'Hair', stock: 19},
@@ -32,3 +29,17 @@ const hairProducts = [
     {id: 'h-7', name: 'Heat Protectant', price: 30.00, category: 'Hair', stock: 28},
     {id: 'h-8', name: 'Round Brush', price: 20.00, category: 'Hair', stock: 33},
 ];
+
+
+export default function ProductList() {
+    return (
+        <div>
+            <h2>Product List</h2>
+            <ul>
+                {products.map((p) => (
+                    <li key={p.id}>{p.name} - ${p.price}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
